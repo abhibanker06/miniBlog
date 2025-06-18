47e2ec45fcc1d8f5092f906ed9fe61804f9e1201
Abhishek <abhishekbanker971@gmail.com>
2025-06-18 22:34:54 +0530

    allow all IPs

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+
+app.set('trust proxy', true);
+
+
 app.use(cors());
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
