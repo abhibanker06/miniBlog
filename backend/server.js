378f558af0790bae0dfe33d55d4e2624331e351b
Abhishek Kumar <abhishekbanker971@gmail.com>
2025-07-05 21:55:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ const app = express();
 app.set('trust proxy', true);
 
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://miniblog-okfa.onrender.com',
+  credentials: true
+}));
+app.options('*', cors());
+
 app.use(express.json({ limit: '20mb' }));
 app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
