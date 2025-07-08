6187f1064433f9f0470c3463e9d56dd984f8944b
Abhishek Kumar <abhishekbanker971@gmail.com>
2025-07-09 02:34:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 
